f6fce9f707dea75b1142386328114aa7a7518214
AdrianBecerra411 <71723297+AdrianBecerra411@users.noreply.github.com>
2022-09-24 18:04:29 -0500

    Fix Img Problem

--- fresh-co-web/src/Pages/Feed.js
+++ fresh-co-web/src/Pages/Feed.js
@@ -6,7 +6,7 @@ function BasicExample() {
         <ProductCard 
             Nombre = "Tomate"
             Precio = "20"
-            ImagenURL = "https://gumlet.https://via.placeholder.com/50://via.placeholder.com/50.com/https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.wikipedia.org%2Fwiki%2FTomate&psig=AOvVaw0npDBFAWSxRZ942fBFYZqT&ust=1664146177494000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCJiJteDBrvoCFQAAAAAdAAAAABAD%2Fimport%2F2018%2F05%2Fgrid-noticias%2Fmichoacan-salud%2F11%2Ftomate.jpg"
+            ImagenUrl = "https://gumlet.assettype.com/mimorelia%2Fimport%2F2018%2F05%2Fgrid-noticias%2Fmichoacan-salud%2F11%2Ftomate.jpg"
             Descripcion = "Tomate verde organico"
             CantidadPrecioKilos = "1"
             CantidadExistenteKilos = "100"
