e380fd5a01163bc9272f0ce55074b6e54507b0f4
Int-Angel <Padilla.angel88@gmail.com>
2022-09-24 17:27:06 -0500

    Update ProductCard.js

---
+++ fresh-co-web/src/Components/ProductCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+export default function ProductCard({
+  Nombre,
+  Precio,
+  ImagenUrl,
+  Descripcion,
+  CantidadPrecioKilos,
+  CantidadExistenteKilos,
+}) {
+  return (
+    <Card>
+      <Card.Img variant="left" src={ImagenUrl} />
+      <Card.Body>
+        <Card.Title>{Nombre}</Card.Title>
+        <Card.Text>{Descripcion}</Card.Text>
+        <Card.Text>
+          Precio = ${Precio} por {CantidadPrecioKilos} Kilos. Quedan{" "}
+          {CantidadExistenteKilos} de kilos de este producto.
+        </Card.Text>
+        <Button variant="primary">+</Button>
+      </Card.Body>
+    </Card>
+  );
+}
