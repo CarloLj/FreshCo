e3c8473cb9eee821b90931bb6425a0a68d18c782
AdrianBecerra411 <71723297+AdrianBecerra411@users.noreply.github.com>
2022-09-24 17:26:52 -0500

    Feed and Details Link Page
    Linked Feed and Details to App.JS

--- fresh-co-web/src/App.js
+++ fresh-co-web/src/App.js
@@ -1,8 +1,22 @@
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { Fragment } from "react";
 import "./App.css";
+import Feed from "./Pages/Feed";
+import Details from "./Pages/Details";
+
+
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+        <Fragment>
+            <Routes>
+                <Route exact path='/Feed' element = {<Feed/>}/>
+                <Route exact path='/Details' element = {<Details/>}/>
+            </Routes>
+        </Fragment>
+    </Router>
+);
 }
 
 export default App;
