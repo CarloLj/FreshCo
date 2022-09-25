5c909a7919d2039411ca6a1d2d9fd25891e37880
Int-Angel <Padilla.angel88@gmail.com>
2022-09-24 19:09:38 -0500

    tailwind

--- fresh-co-web/src/Components/Parcela.js
+++ fresh-co-web/src/Components/Parcela.js
@@ -1,5 +1,61 @@
 import React from "react";
+import ProductCard from "./ProductCard";
+import ListGroup from "react-bootstrap/ListGroup";
+import Card from "react-bootstrap/Card";
 
-export default function Parcela({}) {
-  return <div></div>;
+export default function Parcela({
+  Nombre,
+  Descripcion,
+  ImagenUrl,
+  Locacion,
+  Productos,
+}) {
+  return (
+    <Card>
+      <Card.Body>
+        <ListGroup horizontal>
+          <ListGroup.Item>
+            <ProductCard
+              Nombre="Tomate"
+              Precio="20"
+              ImagenUrl="https://gumlet.assettype.com/mimorelia%2Fimport%2F2018%2F05%2Fgrid-noticias%2Fmichoacan-salud%2F11%2Ftomate.jpg"
+              Descripcion="Tomate verde organico"
+              CantidadPrecioKilos="1"
+              CantidadExistenteKilos="100"
+            />
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <ProductCard
+              Nombre="Tomate"
+              Precio="20"
+              ImagenUrl="https://gumlet.assettype.com/mimorelia%2Fimport%2F2018%2F05%2Fgrid-noticias%2Fmichoacan-salud%2F11%2Ftomate.jpg"
+              Descripcion="Tomate verde organico"
+              CantidadPrecioKilos="1"
+              CantidadExistenteKilos="100"
+            />
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <ProductCard
+              Nombre="Tomate"
+              Precio="20"
+              ImagenUrl="https://gumlet.assettype.com/mimorelia%2Fimport%2F2018%2F05%2Fgrid-noticias%2Fmichoacan-salud%2F11%2Ftomate.jpg"
+              Descripcion="Tomate verde organico"
+              CantidadPrecioKilos="1"
+              CantidadExistenteKilos="100"
+            />
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <ProductCard
+              Nombre="Tomate"
+              Precio="20"
+              ImagenUrl="https://gumlet.assettype.com/mimorelia%2Fimport%2F2018%2F05%2Fgrid-noticias%2Fmichoacan-salud%2F11%2Ftomate.jpg"
+              Descripcion="Tomate verde organico"
+              CantidadPrecioKilos="1"
+              CantidadExistenteKilos="100"
+            />
+          </ListGroup.Item>
+        </ListGroup>
+      </Card.Body>
+    </Card>
+  );
 }
--- fresh-co-web/src/Pages/Feed.js
+++ fresh-co-web/src/Pages/Feed.js
@@ -1,18 +1,17 @@
-import ProductCard from '../Components/ProductCard.js';
-
-
+import Parcela from "../Components/Parcela.js";
+import ProductCard from "../Components/ProductCard.js";
 
 function BasicExample() {
   return (
     <div>
-        <ProductCard 
-            Nombre = "Tomate"
-            Precio = "20"
-            ImagenUrl = "https://gumlet.assettype.com/mimorelia%2Fimport%2F2018%2F05%2Fgrid-noticias%2Fmichoacan-salud%2F11%2Ftomate.jpg"
-            Descripcion = "Tomate verde organico"
-            CantidadPrecioKilos = "1"
-            CantidadExistenteKilos = "100"
-        />
+      <ProductCard
+        Nombre="Tomate"
+        Precio="20"
+        ImagenUrl="https://gumlet.assettype.com/mimorelia%2Fimport%2F2018%2F05%2Fgrid-noticias%2Fmichoacan-salud%2F11%2Ftomate.jpg"
+        Descripcion="Tomate verde organico"
+        CantidadPrecioKilos="1"
+        CantidadExistenteKilos="100"
+      />
     </div>
   );
 }
